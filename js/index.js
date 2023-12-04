c39f30df6a4ca884d3585008c0d133ccd362c2dc
ppoliani <ppoliani@gmail.com>
2023-12-04 15:29:52 +0200

    chore: update dummy package

--- js/index.js
+++ js/index.js
@@ -46,7 +46,7 @@ const main = () => {
 
   console.log({offset_0, offset_1, offset_2});
   
-  const startIndex = data.jwt.indexOf(offset_0);
+  const startIndex = data.jwt.indexOf(offset_1);
 
   console.log('>>>>', startIndex)
 
